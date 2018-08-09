ff4da5075ed1c83285e0312c89d56ae979500425
cocopop1106 <zhzh1300>
2018-08-09 17:28:42 +0900

    alertmanager-store-mariadb

---
+++ manifests/alertmanager-store-mariadb/mariadb-ddl.sql
@@ -0,0 +1,21 @@
+CREATE
+	TABLE
+		history(
+			seq INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			alert JSON,
+			datetime VARCHAR(30)
+		);
+
+SET GLOBAL event_scheduler = ON;
+SET @@global.event_scheduler = ON;
+SET GLOBAL event_scheduler = 1;
+SET @@global.event_scheduler = 1;
+
+CREATE EVENT IF NOT EXISTS history
+	ON SCHEDULE
+		EVERY 1 DAY
+		STARTS CURRENT_TIMESTAMP
+	DO
+		DELETE FROM history
+		WHERE date_format(datetime, '%Y-%m-%d') <= date_sub(curdate(), INTERVAL 1 MONTH)
+;
