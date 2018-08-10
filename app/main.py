42d42271f5c58e83b607b41a5a9d71bb95d8f6dd
billygoo <sangmo.gu@gmail.com>
2018-08-10 10:23:26 +0900

    Add function to crate and intialize a database, and update kubernetes's files. - Set taint&tolerations for isolation - Added prefix to the k8s's resource name : zcp-

--- app/main.py
+++ app/main.py
@@ -1,56 +1,109 @@
 from flask import Flask, request, abort, jsonify
 import json
-import datetime, time
+import datetime
 import pymysql.cursors
 
-# MARIA_SERVER = '169.56.100.62'
-# MARIA_PORT = 30000
-# MARIA_SERVER = 'webhook-mariadb'
+# Dev
+# MARIA_SERVER = '127.0.0.1'
+# MARIA_PORT = 3306
+# MARIA_USER = 'root'
+# MARIA_PWD = 'my-secret-pw'
+
+# Production
+# TODO : Extract to configmap or argument
 MARIA_SERVER = 'alertmanager-store-mariadb'
 MARIA_PORT = 3306
-MARIA_DB = 'alertmanager'
+MARIA_DB_NAME = 'alertmanager'
 MARIA_USER = 'root'
 MARIA_PWD = 'admin'
 
 app = Flask(__name__)
 
+
+def _get_connection(db=None):
+    conn = pymysql.connect(host=MARIA_SERVER,
+                           port=MARIA_PORT,
+                           user=MARIA_USER,
+                           db=db,
+                           password=MARIA_PWD,
+                           charset='utf8')
+    return conn
+
+
+'''
+DB Scheme infomation 
+--------------------
+'''
+
+_SQL_CREATE_DB = 'CREATE DATABASE IF NOT EXISTS %s' % MARIA_DB_NAME
+_SQL_CREATE_TABLE = '''CREATE TABLE IF NOT EXISTS history(
+    seq INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    alert JSON,
+    datetime VARCHAR(30)
+)'''
+_SQL_SET_EVENT_SCHEDULER = '''SET GLOBAL event_scheduler = ON;
+SET @@global.event_scheduler = ON;
+SET GLOBAL event_scheduler = 1;
+SET @@global.event_scheduler = 1;'''
+
+_SQL_CREATE_EVENT = '''CREATE EVENT IF NOT EXISTS history
+ON SCHEDULE
+    EVERY 1 DAY
+    STARTS CURRENT_TIMESTAMP
+DO
+    DELETE FROM history
+    WHERE date_format(datetime, '%Y-%m-%d') <= date_sub(curdate(), INTERVAL 1 MONTH)
+'''
+
+_SQL_INSERT_HISTORY = "INSERT INTO history (alert, datetime) VALUES (%s, %s)"
+
+
+@app.before_first_request
+def _init_db():
+    print '# Check db scheme...'
+    conn = _get_connection()
+    try:
+        with conn.cursor() as cursor:
+            # Create DB
+            cursor.execute(_SQL_CREATE_DB)
+            # Create table and event
+            cursor.execute('USE %s' % MARIA_DB_NAME)
+            cursor.execute(_SQL_CREATE_TABLE)
+            for line in _SQL_SET_EVENT_SCHEDULER.splitlines():
+                cursor.execute(line)
+            cursor.execute(_SQL_CREATE_TABLE)
+        conn.commit()
+    finally:
+        conn.close()
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook_save():
     if request.method == 'POST':
         req_data = request.get_json()
-        # print(req_data)
+
+        # TODO: Verify request data.
+        ##
+
         json_str = json.dumps(req_data)
-        # print(json_str)
 
         now = datetime.datetime.now()
         now_datetime = now.strftime('%Y-%m-%d %H:%M:%S')
-        # print(now_datetime)
 
-        conn = pymysql.connect(host=MARIA_SERVER,
-                               port=MARIA_PORT,
-                               user=MARIA_USER,
-                               password=MARIA_PWD,
-                               db=MARIA_DB,
-                               charset='utf8')
+        conn = _get_connection(MARIA_DB_NAME)
+
         try:
             with conn.cursor() as cursor:
-                sql = 'INSERT INTO history (alert, datetime) VALUES (%s, %s)'
-                cursor.execute(sql, (json_str, now_datetime))
+                cursor.execute(_SQL_INSERT_HISTORY, (json_str, now_datetime))
 
-                conn.commit()
-
-            # with conn.cursor() as cursor:
-            #     sql = 'SELECT * FROM history'
-            #     cursor.execute(sql)
-            #     result = cursor.fetchall()
-            #     print(result)
+            conn.commit()
         finally:
             conn.close()
-
     else:
         abort(400)
 
     return jsonify({'status': 'success'}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
